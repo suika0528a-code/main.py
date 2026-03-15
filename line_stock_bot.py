00d3e844a32764aa2089a540144cf089e95eb4c4
suika0528a-code <suika0528a@gmail.com>
2026-03-15 12:23:55 +0800

    Update line_stock_bot.py

--- line_stock_bot.py
+++ line_stock_bot.py
@@ -1,40 +1,41 @@
 from flask import Flask, request
-from linebot.v3.messaging import MessagingApi, Configuration, ApiClient, ReplyMessageRequest, TextMessage
-from linebot.v3.webhook import WebhookHandler
-from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import yfinance as yf
 import os
 
 app = Flask(__name__)
 
+LINE_CHANNEL_ACCESS_TOKEN = "9M9gWuDL+9swPRQtYnGcAfXFwwy1rwlI2b9gBXBhmKv/5yXkdvAMWKCzptxJu0E9paX1nkj88rDh9HUk0mXCvKznM3sTGM9k6upXohJb/+I/civXiL3dU6dp/1Rpz2X+vPvAhMGj5PE5p0L1pCPR7gdB04t89/1O/w1cDnyilFU="
+LINE_CHANNEL_SECRET = "f7f7b714907d5efd93a29d5866c9593b"
 
-LINE_CHANNEL_ACCESS_TOKEN = "BPQ/+EOBTi0a5fWsab/05yvB8J8v4jsh3zqjk2TqnSoMJ5CsJxU2+RTGKlQx0FndpaX1nkj88rDh9HUk0mXCvKznM3sTGM9k6upXohJb/+JtLYzFboHjcT41gIldo8TNka3g0m8jfe/dgZuU8ll8tAdB04t89/1O/w1cDnyilFU="
-LINE_CHANNEL_SECRET = "23cd049315c1c76fc3c115445fe1f650"
-
-configuration = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    
-    signature = request.headers.get('X-Line-Signature')
-    body = request.get_data(as_text=True)
 
     try:
-        handler.handle(body, signature)
-    except Exception as e:
-        print("Webhook error:", e)
+        signature = request.headers.get('X-Line-Signature')
+        body = request.get_data(as_text=True)
 
+        handler.handle(body, signature)
+
+    except Exception as e:
+        print("Webhook Error:", e)
+
+    # 一定要回傳200
     return "OK"
 
 
-@handler.add(MessageEvent, message=TextMessageContent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
     user_text = event.message.text.strip().upper()
+    print("收到:", user_text)
 
-    print("收到訊息:", user_text)
+    msg = ""
 
     try:
         stock = yf.Ticker(user_text)
@@ -43,20 +44,19 @@ def handle_message(event):
         if price:
             msg = f"{user_text} 目前價格: ${round(price,2)}"
         else:
-            msg = "查不到此股票\n請輸入例如：NVDA / TSLA / AAPL"
+            msg = "查不到股票\n例如: TSLA / NVDA / AAPL"
 
     except Exception as e:
-        print("股票查詢錯誤:", e)
+        print("股票錯誤:", e)
         msg = "股票查詢失敗"
 
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
+    try:
         line_bot_api.reply_message(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=msg)]
-            )
+            event.reply_token,
+            TextSendMessage(text=msg)
         )
+    except Exception as e:
+        print("LINE回覆錯誤:", e)
 
 
 if __name__ == "__main__":
