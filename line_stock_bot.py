67c33ab28d078a8fdb5ab66b0cd66fb399f9cc04
suika0528a-code <suika0528a@gmail.com>
2026-03-15 18:21:32 +0800

    Update line_stock_bot.py

--- line_stock_bot.py
+++ line_stock_bot.py
@@ -2,8 +2,6 @@ from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import yfinance as yf
-import threading
-import time
 
 app = Flask(__name__)
 
@@ -13,7 +11,48 @@ LINE_CHANNEL_SECRET = "f7f7b714907d5efd93a29d5866c9593b"
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-alerts = {}
 
-# ----------------
-# 根
+@app.route("/")
+def home():
+    return "LINE BOT RUNNING"
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+
+    signature = request.headers.get('X-Line-Signature')
+    body = request.get_data(as_text=True)
+
+    try:
+        if signature:
+            handler.handle(body, signature)
+    except:
+        pass
+
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    text = event.message.text.strip().upper()
+
+    try:
+
+        stock = yf.Ticker(text)
+        price = stock.fast_info["last_price"]
+
+        msg = f"{text} 目前價格 ${round(price,2)}"
+
+    except:
+
+        msg = "請輸入股票代碼，例如 NVDA"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=msg)
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
